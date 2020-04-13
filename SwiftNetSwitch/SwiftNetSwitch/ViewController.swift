2a71af1c01a671b04a45651be017c1c846ee9cff
张金虎 <zhangjinhu@36kr.com>
2020-04-13 15:28:28 +0800

    添加示例代码

--- SwiftNetSwitch/SwiftNetSwitch/ViewController.swift
+++ SwiftNetSwitch/SwiftNetSwitch/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//       let sign = SwitchConfig.shared.getSelectNetworkSign()
-//       let config = SwitchConfig.shared.getSelectNetworkConfig()
-//        print("sin--\(String(describing: sign))--config:\(String(describing: config))")
+       let sign = SwitchManager.shared.getSelectNetworkSign()
+       let config = SwitchManager.shared.getSelectNetworkConfig()
+        print("sin--\(String(describing: sign))--config:\(String(describing: config))")
 //
 //        SwitchConfig.shared.configWithFrame(self, CGRect.init(x: 100, y: 100, width: 100, height: 50)) {
 //            print("完成")
